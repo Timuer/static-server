612f03b19a3f1e6332f496343ff36341867cc20c
Timuer <m15927177412@163.com>
2018-07-18 00:13:11 +0800

    refactor processPath file

--- helper/processPath.js
+++ helper/processPath.js
@@ -11,6 +11,34 @@ const readdir = promisify(fs.readdir)
 
 let str = fs.readFileSync('index.ejs', 'utf8')
 
+let retFile = function(req, res, path) {
+    res.statusCode = 200
+    res.setHeader('content-type', mimetypes(path))
+    let rs = fs.createReadStream(path)
+    if (path.match(config.compress)[1]) {
+        rs = compress(req, res, rs)
+    }
+    rs.pipe(res)
+}
+
+let retDirList = function(req, res, path, files) {
+    let result = ejs.render(str, {
+        title: 'Static browser',
+        base: relative(config.root, path),
+        lst: files
+    })
+    res.statusCode = 200
+    res.setHeader('content-type', 'text/html')
+    res.write(result)
+}
+
+let retError = function(req, res) {
+    res.statusCode = 500
+    res.setHeader('content-type', 'text/plain')
+    res.end('Server Internal Error')
+    console.error(ex)
+}
+
 module.exports = async function(req, res, path) {
     if (!fs.existsSync(path)) {
         res.statusCode = 404
@@ -21,30 +49,13 @@ module.exports = async function(req, res, path) {
     let status = await stat(path)
     try {
         if (status.isFile()) {
-            res.statusCode = 200
-            res.setHeader('content-type', mimetypes(path))
-            let rs = fs.createReadStream(path)
-            if (path.match(config.compress)[1]) {
-                rs = compress(req, res, rs)
-            }
-            rs.pipe(res)
+            retFile(req, res, path)
         } else if (status.isDirectory()) {
             let files = await readdir(path)
-            let result = ejs.render(str, {
-                title: 'Static browser',
-                base: relative(config.root, path),
-                lst: files
-            })
-            res.statusCode = 200
-            res.setHeader('content-type', 'text/html')
-            res.write(result)
-            res.end()
+            retDirList(req, res, path, files)
         }    
     } catch(ex) {
-        res.statusCode = 500
-        res.setHeader('content-type', 'text/plain')
-        res.end('Server Internal Error')
-        console.error(ex)
+        retError(req, res)
     }
 }
 
